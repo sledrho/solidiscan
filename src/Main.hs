c34842964108258fba46a149f9563ed568f7a382
sledrho <rory.sheldon@live.co.uk>
2018-02-04 22:42:28 +0000

    Modified main so that the ParseTree appears in proper order.

--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,9 @@ module Main where
 import Parser
 import Lexer
 
-s = "pragma solidity;contract ident_1234 { }"
+s = "pragma solidity; contract ident_1234 { }"
 main :: IO ()
 main = do 
   inStr <- getContents
   let parseTree = solidiscan (alexScanTokens2 inStr)  
-  putStrLn ("Parse Tree: " ++ show(parseTree))
-  print "Finished."
+  putStrLn ("parseTree: " ++ show(reverse(parseTree)))
