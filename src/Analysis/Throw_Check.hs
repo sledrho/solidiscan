e649f23599364b3093158a340a703d5d0bda8457
sledrho <rory.sheldon@live.co.uk>
2018-04-16 02:40:17 +0100

    Updating throw check to handle fall back functions.

--- src/Analysis/Throw_Check.hs
+++ src/Analysis/Throw_Check.hs
@@ -15,15 +15,14 @@ funcThrowCheck inp = do
   funcCheck <- fmap internalThrowCheck concatFunc
   return funcCheck
 
-testx inp = resultCleaner $ fmap internalThrowCheck $ concat $ map funcContInfo $ map functionGetter $ contractContentsGetter inp
-
 -- funcConts is a getter for the contents of a function, it returns a list of tuples containing the expressions in
 -- a functions block and the identifier of the function. 
 -- ? StripBlock is used on the head of the Expression list to remove the blockStatements Expression Type
--- funcContInfo :: [FunctionDef] -> [([Expression], Identifier)]
+funcContInfo :: [FunctionDef] -> [([Expression], Identifier)]
 funcContInfo []  = []
 funcContInfo (x:xs) = case x of
-    (FunctionDef i _ _ _ e) -> (stripBlock(head (e)),i) : funcContInfo xs
+    (FunctionDef i _ _ _ e) -> (stripBlock(head (e)), i) : funcContInfo xs
+    (FallBackFunc _ _ e) -> (stripBlock(head (e)), (Identifier "FallBack Function")) : funcContInfo xs
 
 -- internalThrowCheck takes a tuple of expression and identifier and returns a maybe of that tuple if
 -- the contents of the expression contains a throw statement
