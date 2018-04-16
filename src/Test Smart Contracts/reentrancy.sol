49b9f6a1b970ceed0583fa2b7cce6793e2f98bc8
sledrho <rory.sheldon@live.co.uk>
2018-04-16 01:32:56 +0100

    Grabbing some test contracts  from EtherScan.

---
+++ src/Test Smart Contracts/reentrancy.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.4.8;
+
+contract re_entrancy {
+  mapping (address => uint) public balances;
+
+  function put_func() payable {
+    balances[msg.sender] = msg.value;
+  }
+
+  function get_func() {
+    if (!msg.sender.call.value(balances[msg.sender])()) {
+      throw;
+    }
+      balances[msg.sender] = 0;
+  }
+}
+
+pragma solidity ^0.4.0; 
+contract test {
+  mapping (address => uint) public balances; 
+  
+  function test() {
+    if (!msg.sender.call.value(balances[msg.sender])()) {
+      throw;
+    } 
+    balances[msg.sender] = 0;}
+  } 
+  
+contract test2 {
+  mapping (address => uint) public balances;
+} 
+
+pragma solidity ^0.4.0;
+  
+contract test {
+  mapping (address => uint) public balances; 
+  
+  function test() {
+    if (!msg.sender.call.value(balances[msg.sender])()) 
+    { 
+      throw;
+    } 
+    balances[msg.sender] = 0;
+  } 
+} 
+  
+contract test2 { 
+  mapping (address => uint) public balances; 
+  function test2() { if(true++) 
+  { 
+    throw;
+    }
+  }
+}
