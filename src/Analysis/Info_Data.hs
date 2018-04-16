ded79ce5567756556dd6d2d611d8ecf3307989f5
sledrho <rory.sheldon@live.co.uk>
2018-04-16 01:10:18 +0100

    Updated info with a blank data type.

--- src/Analysis/Info_Data.hs
+++ src/Analysis/Info_Data.hs
@@ -4,6 +4,7 @@ module Analysis.Info_Data where
 data Info = Info Location Details
           | Warning Location Details
           | High Location Details
+          | Blank
             deriving (Show, Eq, Ord)
 type Location = String
 type Details = String
